f65b8dbd1983852503db0f04447b1beef9f11b9c
Thierry de Pauw <thierry@thinkinglabs.io>
2018-12-28 21:08:31 +0100

    TimesheetConsoleRenderer: print dates locale aware and use the python 3 f-string for formatting

--- tests/test_timesheet_console_renderer.py
+++ tests/test_timesheet_console_renderer.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 
 import sys
 import io
+import locale
 from datetime import date
 
 from togglu.timesheet_console_renderer import TimesheetConsoleRenderer
@@ -14,21 +15,27 @@ class TestTimesheetConsoleRendererTest(unittest.TestCase):
 
     @patch('togglu.list_timesheet.ListTimesheet')
     def test_render(self, list_timesheet):
+        
+        default_time_locale = locale.getlocale(locale.LC_TIME)[0]
 
         try:
+            locale.setlocale(locale.LC_TIME, 'fr_BE')
             actual_output = io.StringIO()
             sys.stdout = actual_output
             list_timesheet.execute.return_value = TimesheetResponse([
                 TimesheetDateEntryResponse(date.fromisoformat("2018-12-27"), [
-                    TimesheetClientEntryResponse("enicious", 4 * 1000*60*60)
+                    TimesheetClientEntryResponse("enicious", 2.5 * 1000*60*60)
                 ])
             ])
 
             sut = TimesheetConsoleRenderer(list_timesheet)
             sut.render()
 
-            expected_output = "2018-12-27      enicious      4\n"
+            expected_output = \
+                "27.12.2018 | enicious                       |        2.5\n"
             self.assertEqual(actual_output.getvalue(), expected_output)
 
         finally:
             sys.stdout = sys.__stdout__
+
+            locale.setlocale(locale.LC_TIME, default_time_locale)
--- togglu/timesheet_console_renderer.py
+++ togglu/timesheet_console_renderer.py
@@ -11,5 +11,5 @@ class TimesheetConsoleRenderer():
         
         for date_entry in result.entries:
             for client_entry in date_entry.entries:
-                print("%s      %s      %d" % (date_entry.date, client_entry.client_name, client_entry.duration / 1000/60/60))
+                print(f'{date_entry.date.strftime("%x"):<10} | {client_entry.client_name:<30} | {client_entry.duration / 1000/60/60:10.2n}')
         
