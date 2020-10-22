48a3b532a47fd3500cf334c5d79b3df64f6f93fc
Thierry de Pauw <thierry@thinkinglabs.io>
2020-10-22 22:47:42 +0200

    Fix failing CLI usage tests

--- tests/test_togglu.py
+++ tests/test_togglu.py
@@ -30,9 +30,9 @@ class TestCLI(unittest.TestCase):
 
         except SystemExit:
             expected_output = \
-                "usage: togglu.py [-h] [--toggl-url TOGGL_URL] [--reports-url REPORTS_URL]\n" \
-                "                 {workspaces,timesheet} ...\n" \
-                "togglu.py: error: the following arguments are required: subcommand\n"
+                'usage: togglu.py [-h] [--toggl-url TOGGL_URL] [--reports-url REPORTS_URL]' \
+                ' {workspaces,timesheet} ...\n' \
+                'togglu.py: error: the following arguments are required: subcommand\n'
 
             self.assertEqual(actual_output.getvalue(), expected_output)
 
@@ -49,9 +49,9 @@ class TestCLI(unittest.TestCase):
 
         except SystemExit:
             expected_output = \
-                'usage: togglu.py timesheet [-h] --workspace-id WORKSPACE_ID [--since SINCE]\n' \
-                '                           [--until UNTIL] [--client-id CLIENT_ID]\n' \
-                '                           [--tag-id TAG_ID]\n' \
+                'usage: togglu.py timesheet [-h] --workspace-id WORKSPACE_ID [--since SINCE]' \
+                ' [--until UNTIL] [--client-id CLIENT_ID]' \
+                ' [--tag-id TAG_ID]\n' \
                 'togglu.py timesheet: error: the following arguments are required: --workspace-id\n'
                 
             self.assertEqual(actual_output.getvalue(), expected_output)
